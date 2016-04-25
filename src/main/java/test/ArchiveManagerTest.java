e8a54aeb473253d326ef9a7575147d5170272b3e
WishSummer <summerdaynevergone@gmail.com>
2016-04-25 21:17:33 +0800

    add initial test

---
+++ src/main/java/test/ArchiveManagerTest.java
@@ -0,0 +1,84 @@
+package test;
+
+import org.junit.Test;
+import wheellllll.performance.ArchiveManager;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by summer on 4/25/16.
+ */
+public class ArchiveManagerTest {
+
+    @Test
+    public void testSetInitialDelay() throws Exception {
+
+    }
+
+    @Test
+    public void testSetInterval() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDatePattern() throws Exception {
+
+    }
+
+    @Test
+    public void testStart() throws Exception {
+
+    }
+
+    @Test
+    public void testStop() throws Exception {
+
+    }
+
+    @Test
+    public void testAddLogger() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveLogger() throws Exception {
+
+    }
+
+    @Test
+    public void testSetArchiveDir() throws Exception {
+        ArchiveManager manager = new ArchiveManager();
+        String dir = "test";
+        manager.setArchiveDir(dir);
+        assertEquals(manager.getArchiveDir(),dir);
+    }
+
+    @Test
+    public void testSetArchivePrefix() throws Exception {
+        ArchiveManager manager = new ArchiveManager();
+        String prefix = "test";
+        manager.setArchivePrefix(prefix);
+        assertEquals(manager.getArchivePrefix(),prefix);
+    }
+
+    @Test
+    public void testGetArchiveDir() throws Exception {
+        ArchiveManager manager = new ArchiveManager();
+        String dir = "test";
+        manager.setArchiveDir(dir);
+        assertEquals(manager.getArchiveDir(),dir);
+    }
+
+    @Test
+    public void testGetArchivePrefix() throws Exception {
+        ArchiveManager manager = new ArchiveManager();
+        String prefix = "test";
+        manager.setArchivePrefix(prefix);
+        assertEquals(manager.getArchivePrefix(),prefix);
+    }
+
+    @Test
+    public void testGetArchiveSuffix() throws Exception {
+
+    }
+}

37930974636b56d5b84d711dc7e1fa5fbd44bad4
Kris_Chan <743628145@qq.com>
2016-05-01 00:14:55 +0800

    1.设置一个提交限制，7次归档一次；

--- src/main/java/wheellllll/performance/ArchiveManager.java
+++ src/main/java/wheellllll/performance/ArchiveManager.java
@@ -25,6 +25,7 @@ public class ArchiveManager {
     private ScheduledExecutorService sc = null;
     private int mInitialDelay = 1;
     private int mPeriod = 1;
+    private int mInterval = 1;
     private TimeUnit mTimeUnit = TimeUnit.MINUTES;
 
     private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH_mm_ss");
@@ -45,6 +46,12 @@ public class ArchiveManager {
         mTimeUnit = timeUnit;
     }
 
+    // 设置归档数目
+    public void setArchivePeriod(int number){
+        mInterval = number;
+    }
+    //用于记载的临时变量
+    int tmp = 0;
     public void setDatePattern(String datePattern) {
         df.applyPattern(datePattern);
     }
@@ -59,6 +66,19 @@ public class ArchiveManager {
             }
 
             File destArchive = new File(mArchiveDir, mArchivePrefix + " " + df.format(new Date()) + "." + mArchiveSuffix);
+            tmp++;
+
+            if(mInterval == tmp){
+                File periodArchive = new File("Sum"+mArchiveDir, mArchivePrefix + "_"+ df.format(new Date()) +"." + mArchiveSuffix);
+                try{
+                    ZipUtil.pack(tmpFolder, periodArchive);
+                    FileUtils.cleanDirectory(tmpFolder);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                tmp = 0;
+            }
             try {
                 ZipUtil.pack(tmpFolder, destArchive);
                 FileUtils.cleanDirectory(tmpFolder);
